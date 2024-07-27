a9bb28d4e62943d2baae46c8b0b65d39c17d3740
jawamad <jawamad@gmail.com>
2024-07-28 00:25:11 +0200

    final version to be tested

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: flmuller <flmuller@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 11:17:20 by flmuller          #+#    #+#             */
-/*   Updated: 2024/07/24 09:29:24 by flmuller         ###   ########.fr       */
+/*   Updated: 2024/07/28 00:25:00 by flmuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	ft_error(int code)
 {
 	if (code == 0)
-		printf("there is an error in arg\n");
+		printf("please verify argument format\n");
 	if (code == 1)
-		printf("there is an error in init\n");
+		printf("please verify argument format\n");
 	if (code == 2)
-		printf("there is an error in atoi\n");
+		printf("please verify argument format\n");
 	if (code == 3)
-		printf("there is an error in atoi last arg\n");
+		printf("please verify argument format\n");
 	if (code == 4)
-		printf("there is an error in init mutex\n");
+		printf("please verify argument format\n");
 }
